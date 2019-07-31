7dc4669bc521d7837b7caa7383d5555f7887150d
KecoaTerbank <46042630+KecoaTerbank@users.noreply.github.com>
2019-08-01 00:18:39 +0700

    Add files via upload

---
+++ js/allscript.js
@@ -0,0 +1,49 @@
+var myjs1 = document.createElement("script");
+myjs1.type = "text/javascript";
+myjs1.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/jquery-3.2.0.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs1);
+
+var myjs2 = document.createElement("script");
+myjs2.type = "text/javascript";
+myjs2.src = "https://oss.maxcdn.com/gh/KecoaTerbank/assets/js/jquery-ui.js";
+document.getElementsByTagName("head")[0].appendChild(myjs2);
+
+var myjs3 = document.createElement("script");
+myjs3.type = "text/javascript";
+myjs3.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/bootstrap.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs3);
+
+var myjs4 = document.createElement("script");
+myjs4.type = "text/javascript";
+myjs4.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/jquery.slicknav.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs4);
+
+var myjs5 = document.createElement("script");
+myjs5.type = "text/javascript";
+myjs5.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/owl.carousel.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs5);
+
+var myjs6 = document.createElement("script");
+myjs6.type = "text/javascript";
+myjs6.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets/js/theme.js";
+document.getElementsByTagName("head")[0].appendChild(myjs6);
+
+var myjs7 = document.createElement("script");
+myjs7.type = "text/javascript";
+myjs7.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/magnific-popup.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs7);
+
+var myjs8 = document.createElement("script");
+myjs8.type = "text/javascript";
+myjs8.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/counterup.js";
+document.getElementsByTagName("head")[0].appendChild(myjs8);
+
+var myjs9 = document.createElement("script");
+myjs9.type = "text/javascript";
+myjs9.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/jquery.waypoints.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs9);
+
+var myjs10 = document.createElement("script");
+myjs10.type = "text/javascript";
+myjs10.src = "https://cdn.jsdelivr.net/gh/KecoaTerbank/assets@master/js/jquery.mb.YTPlayer.min.js";
+document.getElementsByTagName("head")[0].appendChild(myjs10);
